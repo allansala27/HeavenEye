f72b290f00032b398496f21645fb5256a80d6799
Allan Salandanan <allansala27@gmail.com>
2017-07-01 15:00:50 -0700

    set up card info

--- app/screens/SinglePlayer.js
+++ app/screens/SinglePlayer.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
-import { View, Text, Button, ScrollView } from 'react-native';
+import { View, Text, Button, ScrollView, TouchableHighlight, Modal } from 'react-native';
     
 class SinglePlayer extends Component {
+    state = {
+        modalVisible: false,
+    }
+
+    setModalVisible(visible) {
+        this.setState({modalVisible: visible});
+    }
 
     render() {
         return (
-            <View> 
+            <View>
+                <Modal
+                    animationType={"slide"}
+                    transparent={false}
+                    visible={this.state.modalVisible}
+                    onRequestClose={() => {alert("Modal has been closed.")}}
+                >
+                    <View style={{marginTop: 22}}>
+                        <View>
+                            <Text>Card Info</Text>
+                            <TouchableHighlight onPress={() => {
+                                this.setModalVisible(!this.state.modalVisible)
+                            }}>
+                                <Text 
+                                    style={
+                                        {
+                                            fontSize: 30,
+                                            padding: 20,
+                                            backgroundColor: 'red'
+                                        }
+                                }>
+                                    Hide Modal
+                                </Text>
+                            </TouchableHighlight>
+                        </View>
+                    </View>
+                </Modal>
                 <ScrollView horizontal={true}>
-                    <Text 
+                    <TouchableHighlight onPress={() => {
+                        this.setModalVisible(true)
+                    }}>
+                        <Text 
                         style={
                             {
                                 fontSize: 30,
@@ -15,9 +51,13 @@ class SinglePlayer extends Component {
                                 backgroundColor: 'blue'
                             }
                         }>
-                        Card 1
-                    </Text>
-                    <Text 
+                            Card 1
+                        </Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight onPress={() => {
+                        this.setModalVisible(true)
+                    }}>
+                        <Text 
                         style={
                             {
                                 fontSize: 30,
@@ -25,19 +65,13 @@ class SinglePlayer extends Component {
                                 backgroundColor: 'red'
                             }
                         }>
-                        Card 2
-                    </Text>
-                    <Text 
-                        style={
-                            {
-                                fontSize: 30,
-                                padding: 20,
-                                backgroundColor: 'green'
-                            }
-                        }>
-                        Card 3
-                    </Text>
-                    <Text 
+                            Card 2
+                        </Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight onPress={() => {
+                        this.setModalVisible(true)
+                    }}>
+                        <Text 
                         style={
                             {
                                 fontSize: 30,
@@ -45,19 +79,13 @@ class SinglePlayer extends Component {
                                 backgroundColor: 'yellow'
                             }
                         }>
-                        Card 4
-                    </Text>
-                    <Text 
-                        style={
-                            {
-                                fontSize: 30,
-                                padding: 20,
-                                backgroundColor: 'orange'
-                            }
-                        }>
-                        Card 5
-                    </Text>
-                    <Text 
+                            Card 3
+                        </Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight onPress={() => {
+                        this.setModalVisible(true)
+                    }}>
+                        <Text 
                         style={
                             {
                                 fontSize: 30,
@@ -65,8 +93,37 @@ class SinglePlayer extends Component {
                                 backgroundColor: 'purple'
                             }
                         }>
-                        Card 6
-                    </Text>
+                            Card 4
+                        </Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight onPress={() => {
+                        this.setModalVisible(true)
+                    }}>
+                        <Text 
+                        style={
+                            {
+                                fontSize: 30,
+                                padding: 20,
+                                backgroundColor: 'orange'
+                            }
+                        }>
+                            Card 5
+                        </Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight onPress={() => {
+                        this.setModalVisible(true)
+                    }}>
+                        <Text 
+                        style={
+                            {
+                                fontSize: 30,
+                                padding: 20,
+                                backgroundColor: 'green'
+                            }
+                        }>
+                            Card 6
+                        </Text>
+                    </TouchableHighlight>
                 </ScrollView>
             </View>
         );
