eca55255e26b79ad2d22875c6971a5e63b9562a8
Allan Salandanan <allansala27@gmail.com>
2017-07-22 10:32:47 -0700

    timer config

--- app/screens/Game.js
+++ app/screens/Game.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, Button, Image, StyleSheet, StatusBar } from 'react-native';
+import { Animated, Easing } from 'react-native';
 
 import Target from '../components/Target';
 import PlayableCards from '../components/PlayableCards';
@@ -30,23 +31,49 @@ var styles = StyleSheet.create({
     alignItems: 'center',
   }
 })
+const timeLimit = 5000;
+let animateValue = new Animated.Value(timeLimit)
     
 export default class Game extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isGameRunning: false,
+            isGameRunning: true,
             score: 0,
             targetVal: 0,
             chosenVal: 0,
+            animateValue: new Animated.Value(timeLimit)
         } 
+        this.animateValue = new Animated.Value(timeLimit)
         this.scoreRef = database.child('score');
     }
 
-    componentDidMount() {
-        this.setState({
-            isGameRunning: true 
-        })
+     componentDidMount() {
+        this.spin();
+    }
+
+    spin = () => {
+        // this.setState({
+        //     animateValue : this.state.animateValue.setValue(timeLimit)
+        // })
+        this.animateValue.setValue(timeLimit);
+         Animated.timing(this.animateValue, {
+            duration: timeLimit,
+            easing: Easing.linear, // No easing
+            toValue: 0,
+        }).start(()=> {
+            this.spin()
+            console.log('Game Finished!!!!!!')
+        });
+    }
+    componentWillUpdate(){
+
+    }
+
+    getRemainingTime(timeRemaining) {
+        if(JSON.stringify(timeRemaining) === 0) {
+            this.props.navigation.navigate('Score');
+        }
     }
 
     getTargetValue(newTargetVal) {
@@ -59,14 +86,14 @@ export default class Game extends Component {
         this.setState({
             chosenVal: newChosenVal
         })
-        console.log("LOOK AT THIS: chosen Val = " + newChosenVal);
     }
+
 
     render() {
         return (
             <Image source={require('../assets/images/heaveneye_screengame_field.jpg')} style={styles.container}>
             <StatusBar hidden />
-            <Timer /> 
+            <Timer getTime={this.getRemainingTime.bind(this)} animateValue={this.animateValue}/> 
             <Target getTarget={this.getTargetValue.bind(this)}/>
             <PlayableCards chooseValue={this.getChosenValue.bind(this)}>
             </PlayableCards>
